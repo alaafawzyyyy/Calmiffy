c1d18e805f9e33b16520845d0578d612f306d178
alaa shaaban <105722498+alaafawzyyyy@users.noreply.github.com>
2025-05-12 21:34:25 +0300

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,11 +1,13 @@
-
 from pydantic import BaseModel
 
+# Model for creating a note (Input)
 class NoteIn(BaseModel):
-    content: str
+    content: str  # The content of the note
 
+# Model for returning a note (Output) with ID
 class Note(NoteIn):
-    id: int
+    id: int  # The ID of the note, inherited from NoteIn and added to the model
 
+# Model for processing text request (Input for prediction)
 class TextRequest(BaseModel):
-    text: str
+    text: str  # The text that needs to be processed or predicted
